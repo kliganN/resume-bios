15196acd3af726e40d65bb760a0bc106dcb13850
kliganN <sikpro1337@yandex.ru>
2026-03-31 07:02:37 +0300

    Fix BIOS font application on Pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,12 @@ import localFont from 'next/font/local'
 
 const ndsBios = localFont({
   src: '../public/fonts/NDS-Bios.ttf',
-  variable: '--font-nds-bios',
   display: 'swap',
 })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={ndsBios.variable}>
+    <div className={ndsBios.className}>
       <Component {...pageProps} />
     </div>
   )
