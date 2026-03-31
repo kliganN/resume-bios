9c5c12c7576f2407dfff0e173f384e75d26fa94c
kliganN <sikpro1337@yandex.ru>
2026-03-31 06:56:35 +0300

    Add GitHub Pages deployment workflow

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  ...(process.env.PAGES_BASE_PATH ? { basePath: process.env.PAGES_BASE_PATH } : {}),
+}
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import localFont from 'next/font/local'
+
+const ndsBios = localFont({
+  src: '../public/fonts/NDS-Bios.ttf',
+  variable: '--font-nds-bios',
+  display: 'swap',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={ndsBios.variable}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
